a673e1a45914fdd8870ed2edfebcfee2006aff81
Yasya <80125891+yasya-ayesha@users.noreply.github.com>
2021-06-19 10:10:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const board = document.querySelector('#board')
+const colors = ['#ff0000', '#ff6600', '#9900ff', '#0033ff', '#ffff00', '#00ff00', '#ff00ff', '#00ffff']
+const SQUARES_NUMBER = 900
+
+
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
